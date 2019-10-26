77542e2f6ce22e957bd0f384523926bf1b0035dd
CallMeSweeT <52930404+CallMeSweeT@users.noreply.github.com>
2019-10-26 09:01:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,9 +7,16 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'apply') {
-    	message.reply('Thanks For your Application!');message.reply
+    	message.reply('Applied the Discord server, and now ready for the Fam');
   	}
 });
+
+client.on('message', message => {
+    if (message.content === 'Social') {
+    	message.reply('https://www.youtube.com/channel/UCqehLUaBDWE2pU76SgW6eKw?view_as=subscriber');
+  	}
+});
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
