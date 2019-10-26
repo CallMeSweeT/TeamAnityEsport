83ae5b9dda9b03149a8d55fde75f47af7239bc98
CallMeSweeT <52930404+CallMeSweeT@users.noreply.github.com>
2019-10-26 08:47:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'apply') {
-    	message.reply('thanks for your application (massege.content)');
+    	message.reply('Thanks For your Application!');
   	}
 });
 
